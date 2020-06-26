8902f992d54f9c89608619f2c29ca79bcd585f8d
kulkarniad <nrjklk@gmail.com>
2020-06-26 18:59:11 -0400

    frontend upvote functionality

--- app/public/frontendJS/renderPapers.js
+++ app/public/frontendJS/renderPapers.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(myData.fetchURL)
         .then(response => response.json())
         .then(allPapers => {
+            addUpvoteFunctions()
             allPapers.forEach(paper => {
                 renderPaper(paper)
             });
@@ -42,9 +43,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     function renderPaper(paperObject) {
+
         let titleElem = document.createElement("h3");
         let arxivID = paperObject.pdfUrl.split('/').slice(-1).pop()
-        titleElem.innerHTML = `
+        titleElem.innerHTML = ` 
             <a href=/paper/${arxivID}>            
             <h3>${paperObject.title}</h3>
             </a>`
@@ -66,4 +68,53 @@ document.addEventListener("DOMContentLoaded", () => {
         paperSection.appendChild(abstractElem)
         paperSection.appendChild(document.createElement("br"))
     }
+
+    function addUpvoteFunctions() {
+        dbVotes = 10 // Votes from databse
+        let outerDiv = document.createElement('div')
+        let voteElem = document.createElement('p')
+        let upElem = document.createElement('i');
+        let downElem = document.createElement('i');
+
+        upElem.setAttribute('class', "fa fa-arrow-up upvote")
+        upElem.setAttribute('id', "upvote")
+
+        downElem.setAttribute('class', "fa fa-arrow-down downvote")
+        downElem.setAttribute('id', "downvote")
+
+        voteElem.setAttribute('id','vote-score')
+        voteElem.innerText = dbVotes
+
+        outerDiv.appendChild(upElem)
+        outerDiv.appendChild(voteElem)
+        outerDiv.appendChild(downElem)
+        paperSection.appendChild(outerDiv)
+
+
+        function vote(type) {
+            const buttons = { "1": upElem, "-1": downElem };
+            const score = Number(voteElem.textContent);
+
+            if (buttons[type].classList.contains("active")) {
+                voteElem.textContent = score - type;
+                buttons[type].classList.remove("active");
+            } else if (buttons[-type].classList.contains("active")) {
+                voteElem.textContent = score + 2 * type;
+                buttons[-type].classList.remove("active");
+                buttons[type].classList.add("active");
+            } else {
+                voteElem.textContent = score + type;
+                buttons[type].classList.add("active");
+            }
+        };
+
+        upElem.addEventListener("click", () => vote(1));
+        downElem.addEventListener("click", () => vote(-1));
+
+
+    }
+
+
+
 })
+
